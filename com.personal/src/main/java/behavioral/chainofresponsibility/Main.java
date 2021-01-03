1ee502a5dc73b605f1460ab5a38f2a4b1a819882
wanglong16 <wanglong16@meicai.cn>
2021-01-04 00:06:14 +0800

    责任链模式

---
+++ com.personal/src/main/java/behavioral/chainofresponsibility/Main.java
@@ -0,0 +1,28 @@
+package behavioral.chainofresponsibility;
+
+/**
+ * @description:
+ * @version: 1.0
+ * @date: 2021-01-03 22:56:15
+ * @author: wanglong16@meicai.cn
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        UIDesigner uiDesigner = new UIDesigner("小白");
+        CutPictureBoy cutPictureBoy = new CutPictureBoy("切图仔");
+        CRUDBoy crudBoy = new CRUDBoy("crud小菜鸟");
+
+        Work design = new Work(1, "设计网站页面");
+        Work devCss = new Work(2, "调整网站页面");
+        Work devInterface = new Work(3, "开发服务接口");
+
+        uiDesigner.setNextCooperator(cutPictureBoy);
+        cutPictureBoy.setNextCooperator(crudBoy);
+
+        //链式调用对象。。。
+        uiDesigner.handle(design);
+        uiDesigner.handle(devCss);
+        uiDesigner.handle(devInterface);
+    }
+}
---
+++ com.personal/src/main/java/behavioral/chainofresponsibility/Technician.java
@@ -0,0 +1,19 @@
+package behavioral.chainofresponsibility;
+
+/**
+ * @description: 技术人员
+ * @version: 1.0
+ * @date: 2021-01-03 22:24:40
+ * @author: wanglong16@meicai.cn
+ * @summary:
+ */
+public abstract class Technician implements IWorkIng{
+
+    protected Technician nextCooperator;
+
+    public void setNextCooperator(Technician nextCooperator) {
+        this.nextCooperator = nextCooperator;
+    }
+
+    abstract void handle(Work work);
+}

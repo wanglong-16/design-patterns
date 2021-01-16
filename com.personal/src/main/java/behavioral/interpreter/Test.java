7735afac94b3d768a768100f1178d3276dd58f26
wanglong16 <wanglong16@meicai.cn>
2021-01-16 11:48:11 +0800

    interpreter

---
+++ com.personal/src/main/java/behavioral/interpreter/Test.java
@@ -0,0 +1,15 @@
+package behavioral.interpreter;
+
+/**
+ * @description:
+ * @version: 1.0
+ * @date: 2021-01-16 11:31:37
+ * @author: wanglong16@meicai.cn
+ * @summary:
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+}
